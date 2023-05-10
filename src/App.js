f54f2a0cc803995cf0edd52e00afa87b11d43d59
James Henning <jamesniklas@hotmail.de>
2023-05-10 20:08:53 +0200

    Finish header element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import styles from './App.module.scss';
+
 export default function App() {
   return (
     <>
-      <header>my header</header>
+      <header className={styles.header}>
+        <h1>
+          <a href="/a"> Afterlife</a>
+        </h1>
+      </header>
+
       <main>main content</main>
       <footer>my footer</footer>
     </>
