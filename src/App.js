da4fdb8c2e2daadea557300c3011669d81148ee5
James Henning <jamesniklas@hotmail.de>
2023-05-10 21:39:39 +0200

    Improve section 1

--- src/App.js
+++ src/App.js
@@ -8,8 +8,26 @@ export default function App() {
           <a href="/a"> Afterlife</a>
         </h1>
       </header>
+      <main>
+        <div className={styles['top-div']}>
+          <h1>
+            Write, draft, and store your will <br /> in 10 minutes!
+          </h1>
+          <p>
+            Establish a will today on any device and safeguard your life's work
+            in <br />
+            case of the unthinkable.
+          </p>
+          <form className={styles['email-input-container']}>
+            <input type="email"  placeholder="Email" />
+            <button type className="submit-button">
+              GET ACCESS
+            </button>
+            <h6>Limited spots available for early access.</h6>
+          </form>
+        </div>
+      </main>
 
-      <main>main content</main>
       <footer>my footer</footer>
     </>
   );
