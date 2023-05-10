3bb745b50267d62592cae0d85c7ff7e863507e85
James Henning <jamesniklas@hotmail.de>
2023-05-10 22:46:40 +0200

    Move section 1 to position

--- src/App.js
+++ src/App.js
@@ -9,23 +9,25 @@ export default function App() {
         </h1>
       </header>
       <main>
-        <div className={styles['top-div']}>
-          <h1>
-            Write, draft, and store your will <br /> in 10 minutes!
-          </h1>
-          <p>
-            Establish a will today on any device and safeguard your life's work
-            in <br />
-            case of the unthinkable.
-          </p>
-          <form className={styles['email-input-container']}>
-            <input type="email"  placeholder="Email" />
-            <button type className="submit-button">
-              GET ACCESS
-            </button>
-            <h6>Limited spots available for early access.</h6>
-          </form>
-        </div>
+        <section className="Topsection">
+          <div className={styles['top-div']}>
+            <h1>
+              Write, draft, and store your will <br /> in 10 minutes!
+            </h1>
+            <p>
+              Establish a will today on any device and safeguard your life's
+              work in <br />
+              case of the unthinkable.
+            </p>
+            <form className={styles['email-input-container']}>
+              <input type="email" placeholder="Email" />
+              <button type className={styles['submit-button']}>
+                GET ACCESS
+              </button>
+              <h6>Limited spots available for early access.</h6>
+            </form>
+          </div>
+        </section>
       </main>
 
       <footer>my footer</footer>
