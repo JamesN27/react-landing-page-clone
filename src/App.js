0e204bcee24c569d61726d115d4f007593f0f34c
James Henning <jamesniklas@hotmail.de>
2023-05-11 17:03:35 +0200

    Set up second section

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         </h1>
       </header>
       <main>
-        <section className="Topsection">
+        <section className={styles.Topsection}>
           <div className={styles['top-div']}>
             <h1>
               Write, draft, and store your will <br /> in 10 minutes!
@@ -27,6 +27,35 @@ export default function App() {
               <h6>Limited spots available for early access.</h6>
             </form>
           </div>
+        </section>
+        <section className={styles.Middlesection}>
+          <div>
+            <h2>
+              Manage, safeguard, and update the most important information at
+              your fingertips.
+            </h2>
+            <p>
+              Your family's future is too important be left to some stale piece
+              of paper hidden in some box along with winter clothing.
+            </p>
+          </div>
+          <br />
+          <span className="Spanoptions">
+            Manage all assets and liabilities at your fingertips Link your bank
+            accounts, brokerage accounts and loans, add physical assets such as
+            jewelery, land etc, and manage how they should get split up.
+          </span>
+          <br />
+          <span className="Spanoptions">
+            Leave your loved ones in safe hands Assign legal guardianship for
+            your kids. Leave personal notes, images and videos for your loved
+            ones as part of your will.
+          </span>
+          <br />
+          <span className="Spanoptions">
+            Keep your beneficiaries in the know. Catalog and add physical
+            assets, add details including notes, photos, deeds and certificates.
+          </span>
         </section>
       </main>
 
