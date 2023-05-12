a02eae23f17b69f9bf6a09f85a96c987e40cde53
James Henning <jamesniklas@hotmail.de>
2023-05-12 16:08:02 +0200

    Set up section 3

--- src/App.js
+++ src/App.js
@@ -42,27 +42,48 @@ export default function App() {
             </p>
           </div>
           <br />
-          <span className="Spanoptions">
-            Manage all assets and liabilities at your fingertips Link your bank
-            accounts, brokerage accounts and loans, add physical assets such as
-            jewelery, land etc, and manage how they should get split up.
+          <span className={styles.Spanoptions}>
+            <h3>Manage all assets and liabilities at your fingertips </h3>
+            <p>
+              Link your bank accounts, brokerage accounts and loans, add
+              physical <br /> assets such as jewelery, land etc, and manage how
+              they should get split <br /> up.
+            </p>
           </span>
           <br />
-          <span className="Spanoptions">
-            Leave your loved ones in safe hands Assign legal guardianship for
-            your kids. Leave personal notes, images and videos for your loved
-            ones as part of your will.
+          <span className={styles.Spanoptions}>
+            <h3>Leave your loved ones in safe hands </h3>
+            <p>
+              Assign legal guardianship for your kids. Leave personal notes,
+              images and <br /> videos for your loved ones as part of your will.
+            </p>
           </span>
           <br />
-          <span className="Spanoptions">
-            Keep your beneficiaries in the know. Catalog and add physical
-            assets, add details including notes, photos, deeds and certificates.
+          <span className={styles.Spanoptions}>
+            <h3>Keep your beneficiaries in the know</h3>{' '}
+            <p>
+              Catalog and add physical assets, add details including notes,
+              photos,
+              <br /> deeds and certificates.
+            </p>
           </span>
         </section>
-        <section className={styles.Downsection}>third section</section>
+        <section className={styles.Downsection}>
+          <h2>
+            Wills are meant for <span className={styles.Italic}>everyone.</span>
+          </h2>
+          <p>
+            Afterlife allows all Indians to secure their family's future without
+            the need of expensive lawyers.
+          </p>
+        </section>
       </main>
 
       <footer>my footer</footer>
     </>
   );
 }
+
+// display: block;
+//  margin-top: -20px;
+//  margin-bottom: -8px;
